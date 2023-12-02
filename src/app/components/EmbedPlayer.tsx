ea1b77a6fac5a18be6ef33db1e4d0241234ec845
haintse06350 <haint180298@gmail.com>
2023-12-03 00:21:16 +0700

    add dragable player

--- src/app/components/EmbedPlayer.tsx
+++ src/app/components/EmbedPlayer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { FC, HTMLAttributes, useEffect } from "react";
+import React, { FC, HTMLAttributes, useEffect, useRef } from "react";
 import { useAppDispatch, useAppSelector } from "../lib/redux/hooks";
 import { setTrack } from "../lib/redux/slices";
 
@@ -7,18 +7,57 @@ export const EmbedPlayer: FC<HTMLAttributes<HTMLDivElement>> = () => {
   const dispatch = useAppDispatch();
   const { track } = useAppSelector((state) => state.player);
 
+  const ref = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     if (track) {
       document.title = track.name + "-" + track.artists[0].name;
     }
   }, [track]);
 
+  useEffect(() => {
+    let x = 0;
+    let y = 0;
+    const ele = ref.current;
+    if (!ele || !track) return;
+    const mouseDownHandler = function (e: any) {
+      x = e.clientX;
+      y = e.clientY;
+
+      document.addEventListener("mousemove", mouseMoveHandler);
+      document.addEventListener("mouseup", mouseUpHandler);
+    };
+
+    const mouseMoveHandler = function (e: any) {
+      const dx = e.clientX - x;
+      const dy = e.clientY - y;
+
+      ele.style.top = `${ele.offsetTop + dy}px`;
+      ele.style.left = `${ele.offsetLeft + dx}px`;
+
+      x = e.clientX;
+      y = e.clientY;
+    };
+
+    const mouseUpHandler = function () {
+      document.removeEventListener("mousemove", mouseMoveHandler);
+      document.removeEventListener("mouseup", mouseUpHandler);
+    };
+
+    ele.addEventListener("mousedown", mouseDownHandler);
+  }, [track]);
+
   if (!track) return null;
 
   return (
-    <div className="inline-block fixed md:w-1/3 lg:w-1/4 transition ease-in-out duration-500 bottom-0 right-8 z-50 box-border">
+    <div
+      ref={ref}
+      id="player"
+      className="select-none fixed md:w-1/3 lg:w-1/4 transition ease-in-out duration-500 bottom-0 right-8 z-50 box-border"
+    >
+      <i className="opacity-0 hover:opacity-100 select-none bi bi-arrows-move text-gray-300 text-1xl cursor-pointer absolute right-6 -top-7" />
       <i
-        className="bi bi-x-circle-fill text-gray-300 text-1xl cursor-pointer absolute -right-1 -top-3"
+        className="bi bi-x-circle-fill text-gray-300 text-1xl cursor-pointer absolute -right-1 -top-7"
         onClick={() => dispatch(setTrack(undefined))}
       />
       <iframe
