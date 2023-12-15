037c691c451e1543a7add5800f4d98979e2f8cd3
haintse06350 <haint180298@gmail.com>
2023-12-15 23:50:51 +0700

    improve: player

--- src/app/components/EmbedPlayer.tsx
+++ src/app/components/EmbedPlayer.tsx
@@ -6,7 +6,8 @@ import { setTrack } from "../../lib/redux/slices/playerSlices";
 export const EmbedPlayer: FC<HTMLAttributes<HTMLDivElement>> = () => {
   const dispatch = useAppDispatch();
   const { track } = useAppSelector((state) => state.player);
-
+  const [minimized, setMinimized] = React.useState(false);
+  const [dragging, setDragging] = React.useState(false);
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -17,6 +18,7 @@ export const EmbedPlayer: FC<HTMLAttributes<HTMLDivElement>> = () => {
     document.title = track.name + "-" + track.artists[0].name;
 
     const mouseDownHandler = function (e: any) {
+      setDragging(true);
       x = e.clientX;
       y = e.clientY;
 
@@ -25,6 +27,7 @@ export const EmbedPlayer: FC<HTMLAttributes<HTMLDivElement>> = () => {
     };
 
     const mouseMoveHandler = function (e: any) {
+      setDragging(true);
       const dx = e.clientX - x;
       const dy = e.clientY - y;
 
@@ -36,6 +39,7 @@ export const EmbedPlayer: FC<HTMLAttributes<HTMLDivElement>> = () => {
     };
 
     const mouseUpHandler = function () {
+      setDragging(false);
       document.removeEventListener("mousemove", mouseMoveHandler);
       document.removeEventListener("mouseup", mouseUpHandler);
     };
@@ -54,9 +58,24 @@ export const EmbedPlayer: FC<HTMLAttributes<HTMLDivElement>> = () => {
     <div
       ref={ref}
       id="player"
-      className="select-none fixed h-[80px] md:w-1/3 lg:w-1/4 transition ease-in-out duration-500 bottom-0 right-8 z-50 box-border"
+      style={{
+        width: minimized ? "80px" : "",
+        right: minimized ? 0 : 8,
+        left: minimized ? "unset" : "",
+      }}
+      className={`select-none fixed w-[80px] h-[80px] md:w-1/3 lg:w-1/4 ${
+        dragging ? "transition" : "transition-all"
+      }  ease-in-out duration-500 bottom-0 right-8 z-50 box-border`}
     >
-      <i className="opacity-0 hover:opacity-100 select-none bi bi-arrows-move text-gray-300 text-1xl cursor-pointer absolute right-6 -top-7" />
+      <i
+        className={`select-none bi ${
+          minimized ? "bi-arrows-angle-expand" : "bi-dash"
+        } text-gray-300 ${
+          minimized ? "text-sm" : "text-2xl"
+        } font-bold cursor-pointer absolute right-12 -top-7`}
+        onClick={() => setMinimized(!minimized)}
+      />
+      <i className="select-none bi bi-arrows-move text-gray-300 text-md cursor-pointer absolute right-6 -top-7" />
       <i
         className="bi bi-x-circle-fill text-gray-300 text-1xl cursor-pointer absolute -right-1 -top-7"
         onClick={() => dispatch(setTrack(undefined))}
