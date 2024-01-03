591e05c92fadc235899d9d1e212cc8307487899f
haintse06350 <haint180298@gmail.com>
2024-01-03 16:30:23 +0700

    fix: api lyrics

--- src/app/api/lyrics/route.ts
+++ src/app/api/lyrics/route.ts
@@ -4,32 +4,23 @@ import { NextResponse } from "next/server";
 export const GET = async (req: Request, res: Response) => {
   try {
     const client = new Client();
-    if (req.method === "GET") {
-      const url = new URL(req.url);
-      const songName = url.searchParams.get("song");
-      const artistName = url.searchParams.get("artist");
-      try {
-        const searches = await client.songs.search(songName + " " + artistName);
-        const song = searches[0];
-        const lyrics = await song?.lyrics();
-        return NextResponse.json({
-          status: 200,
-          lyrics: lyrics,
-          title: song?.title,
-          artist: song?.artist.name,
-          album: song?.album?.name,
-          albumArt: song?.album?.image,
-          releaseDate: song?.releasedAt,
-          image: song?.image,
-        });
-      } catch (error) {
-        console.log(error);
-        return NextResponse.json({ status: 404, error: "Lyrics not found" });
-      }
-    } else {
-      return NextResponse.json({ status: 400, error: "Method not allowed" });
-    }
+    const url = new URL(req.url);
+    const songName = url.searchParams.get("song");
+    const artistName = url.searchParams.get("artist");
+    const searches = await client.songs.search(songName + " " + artistName);
+    const song = searches[0];
+    const lyrics = await song?.lyrics();
+    return NextResponse.json({
+      status: 200,
+      lyrics: lyrics,
+      title: song?.title,
+      artist: song?.artist.name,
+      album: song?.album?.name,
+      albumArt: song?.album?.image,
+      releaseDate: song?.releasedAt,
+      image: song?.image,
+    });
   } catch (error) {
-    console.log(error);
+    return NextResponse.json({ status: 404, error: "Lyrics not found" });
   }
 };
